4a4cfd026a07710f4548ab8e51282869032199fd
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-10 18:20:15 +0600

    loopback4 simple product done

--- lb-prod/src/migrate.ts
+++ lb-prod/src/migrate.ts
@@ -6,7 +6,7 @@ export async function migrate(args: string[]) {
 
   const app = new StarterApplication();
   await app.boot();
-  await app.migrateSchema({existingSchema});
+  await app.migrateSchema({existingSchema, models: ['Student']});
 
   // Connectors usually keep a pool of opened connections,
   // this keeps the process running even after all work is done.
---
+++ lb-prod/src/models/student.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Student extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Student>) {
+    super(data);
+  }
+}
+
+export interface StudentRelations {
+  // describe navigational properties here
+}
+
+export type StudentWithRelations = Student & StudentRelations;
